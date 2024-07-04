3165c5a082b7697334a43874ff36a6bbad3679d0
Sean <dog0120@163.com>
2024-07-04 19:16:51 -0400

    Time: 1033 ms (75.31%), Space: 0B (100.00%) - LeetHub

--- 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
+++ 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
@@ -1,7 +1,11 @@
 # Write your MySQL query statement below
-SELECT product_id, year AS first_year, quantity, price
-FROM Sales
-WHERE (product_id, year) IN (
-SELECT product_id, MIN(year) as year
-FROM Sales
-GROUP BY product_id) ;
+select product_id, 
+    `year` as first_year, 
+    quantity, 
+    price
+from (
+    select *, 
+        rank() over (partition by product_id order by `year`) as year_order
+    from Sales
+) g
+where year_order=1
