c63b6b4d0dd1d85786e6c41f2a4dfb8296ff8676
Sean <dog0120@163.com>
2024-07-04 19:18:09 -0400

    Time: 1041 ms (74.43%), Space: 0B (100.00%) - LeetHub

--- 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
+++ 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
@@ -1,4 +1,5 @@
 # Write your MySQL query statement below
+# Here using row_number() will be wrong because it seems that for a specific product_id and year, there will be multiple sale_id, and the result doesn't want them to be merged(which needs to be confirmed with the questioner)
 select product_id, 
     `year` as first_year, 
     quantity, 
