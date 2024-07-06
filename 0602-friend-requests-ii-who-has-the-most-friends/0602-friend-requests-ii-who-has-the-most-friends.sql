a50070f1106e6b772434421858bedd4f8d3ceca5
Sean <dog0120@163.com>
2024-07-06 19:55:51 -0400

    Time: 298 ms (70.21%), Space: 0B (100.00%) - LeetHub

--- 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
+++ 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-# one test case is abnormal: [4,3,"2016/06/08"],[3,4,"2016/06/09"], in this case user id 3 only have 1 friend, but the given answer counts to 2, which is incorrect. So use union should be the correct answer
+# one test case is abnormal: [4,3,"2016/06/08"],[3,4,"2016/06/09"], in this case user id 3 only have 1 friend, but the given answer counts to 2, which is incorrect. So use union should be more robust
 select id, count(fid) as num
 from (
     select requester_id as id, accepter_id as fid
