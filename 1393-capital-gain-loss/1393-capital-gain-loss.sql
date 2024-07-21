376c25eb9442572153e885d07e6092143b2796d2
Sean <dog0120@163.com>
2024-07-21 17:22:10 -0400

    Time: 507 ms (48.86%), Space: 0B (100.00%) - LeetHub

---
+++ 1393-capital-gain-loss/1393-capital-gain-loss.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+select stock_name, sum(sell_price - buy_price) as capital_gain_loss
+from (
+    select stock_name, operation, price as buy_price, lead(price) over (partition by stock_name order by operation_day) as sell_price
+    from Stocks
+) g
+where operation = "buy"
+group by stock_name
