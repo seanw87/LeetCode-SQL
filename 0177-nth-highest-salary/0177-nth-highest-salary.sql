7c7e229d9016e59ddf4c558db296513628e320ff
Sean <dog0120@163.com>
2024-07-21 22:23:03 -0400

    Time: 372 ms (89.21%), Space: 0B (100.00%) - LeetHub

--- 0177-nth-highest-salary/0177-nth-highest-salary.sql
+++ 0177-nth-highest-salary/0177-nth-highest-salary.sql
@@ -7,7 +7,7 @@ BEGIN
       # select distinct salary from Employee order by salary desc
       # limit 1 offset N
       
-      # using window function
+      # using dense_rank function
       select distinct g.salary
       from (
           select salary, dense_rank() over (order by salary desc) as salary_rank
