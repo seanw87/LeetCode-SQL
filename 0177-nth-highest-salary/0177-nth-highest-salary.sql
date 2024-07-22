72d91dfe224d7606b733d0cbdd3df2a1c40e2766
Sean <dog0120@163.com>
2024-07-21 22:22:52 -0400

    Time: 459 ms (41.14%), Space: 0B (100.00%) - LeetHub

--- 0177-nth-highest-salary/0177-nth-highest-salary.sql
+++ 0177-nth-highest-salary/0177-nth-highest-salary.sql
@@ -1,18 +1,18 @@
 CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
-  SET N = N - 1;
+  # SET N = N - 1;
   RETURN (
       # Write your MySQL query statement below.
       
-      select distinct salary from Employee order by salary desc
-      limit 1 offset N
+      # select distinct salary from Employee order by salary desc
+      # limit 1 offset N
       
-      # not sure why following sql gives null value
-      # select g.salary
-      # from (
-      #     select salary, row_number() over (partition by id order by salary desc) as salary_rank
-      #     from Employee
-      # ) g
-      # where salary_rank = N
+      # using window function
+      select distinct g.salary
+      from (
+          select salary, dense_rank() over (order by salary desc) as salary_rank
+          from Employee
+      ) g
+      where g.salary_rank = N
   );
 END
