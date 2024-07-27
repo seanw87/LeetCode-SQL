2b2690af931abc8a4daf84c122ab253b60693f22
Sean <dog0120@163.com>
2024-07-26 23:52:10 -0400

    Time: 543 ms (45.80%), Space: 0B (100.00%) - LeetHub

---
+++ 0262-trips-and-users/0262-trips-and-users.sql
@@ -0,0 +1,16 @@
+# Write your MySQL query statement below
+select request_at as Day, round(sum(if(`status`="cancelled_by_driver" or `status`="cancelled_by_client", 1, 0))/sum(1), 2) as `Cancellation Rate`
+from (
+    select a.`status`, a.request_at, b.banned as client_banned, c.banned as driver_banned
+    from (
+        select id, client_id, driver_id, `status`, request_at
+        from Trips
+        where request_at between '2013-10-01' and '2013-10-03'
+    ) a
+    left join Users b
+        on a.client_id = b.users_id
+    left join Users c
+        on a.driver_id = c.users_id
+    where b.banned="No" and c.banned="No"
+) g
+group by request_at
